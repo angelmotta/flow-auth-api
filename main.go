a9611f48e3d9fd6a405a58877ee3bfab83b0b0f7
Angel Motta <angel.motta@utec.edu.pe>
2023-07-05 13:55:25 -0500

    feat: starting

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
 	log.Println("Auth Server: hellow world")
+
+	r := chi.NewRouter()
+
+	r.Post("/api/v1/auth/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
 }
